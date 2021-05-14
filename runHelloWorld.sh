b7c8f0f3d0a20c995c342eca1a7abd3d82861396
LWagner <st148345@stud.uni-stuttgart.de>
2021-05-14 21:01:37 +0200

    Added HelloWorld Scripts

---
+++ runHelloWorld.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mvn clean package -B --file pom.xml "-DskipTests=true" "-Dmaven.javadoc.skip=true" "-Dcheckstyle.skipExec=true"
+
+
+java -jar ./target/misim.jar -a ./Examples/example_architecture_model.json -e ./Examples/example_experiment_chaosmonkey.json
+
