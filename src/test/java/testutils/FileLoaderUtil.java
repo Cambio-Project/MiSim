c608cf6644b533168a20274238fea7653fba822f
LWagner <st148345@stud.uni-stuttgart.de>
2021-09-20 11:25:41 +0200

    added Contracts to utils

--- src/test/java/testutils/FileLoaderUtil.java
+++ src/test/java/testutils/FileLoaderUtil.java
@@ -6,23 +6,29 @@ import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 /**
  * @author Lion Wagner
  */
 public class FileLoaderUtil {
 
-    public static File loadFromTestResources(final String... filepath) {
+    @Contract("null -> fail")
+    public static @NotNull File loadFromTestResources(final String... filepath) {
         Path basePath = Paths.get("src", "test", "resources");
         return loadFromFromFileSystem(basePath.toAbsolutePath().toString(), filepath);
     }
 
-    public static File loadFromExampleResources(final String... filepath) {
+    @Contract("null -> fail")
+    public static @NotNull File loadFromExampleResources(final String... filepath) {
         Path basePath = Paths.get("Examples");
         return loadFromFromFileSystem(basePath.toAbsolutePath().toString(), filepath);
 
     }
 
-    private static File loadFromFromFileSystem(final String first, final String... filepath) {
+    @Contract("_, null -> fail")
+    private static @NotNull File loadFromFromFileSystem(final String first, final String... filepath) {
         if (filepath == null || filepath.length == 0) {
             throw new InvalidPathException("", "Concrete path to resource is missing.");
         }
