bdd873b857b54b2d7e47e4cff5273430da02844e
LWagner <st148345@stud.uni-stuttgart.de>
2021-09-20 07:49:57 +0200

    Added method to load form Examples

--- src/test/java/testutils/FileLoaderUtil.java
+++ src/test/java/testutils/FileLoaderUtil.java
@@ -11,13 +11,26 @@ import java.nio.file.Paths;
  */
 public class FileLoaderUtil {
 
-    public static File loadFromTestUtils(final String... filepath) {
+    public static File loadFromTestResources(final String... filepath) {
+        Path basePath = Paths.get("src", "test", "resources");
+        return loadFromFromFileSystem(basePath.toAbsolutePath().toString(), filepath);
+    }
+
+    public static File loadFromExampleResources(final String... filepath) {
+        Path basePath = Paths.get("Examples");
+        return loadFromFromFileSystem(basePath.toAbsolutePath().toString(), filepath);
+
+    }
+
+    private static File loadFromFromFileSystem(final String first, final String... filepath) {
         if (filepath == null || filepath.length == 0) {
             throw new InvalidPathException("", "Concrete path to resource is missing.");
         }
 
-        Path basePath = Paths.get("src", "test", "resources");
-        return FileSystems.getDefault().getPath(basePath.toAbsolutePath().toString(), filepath).toFile();
+        File file = FileSystems.getDefault().getPath(first, filepath).toFile();
+        if (!file.exists()) {
+            throw new InvalidPathException(file.getAbsolutePath(), "Cannot find resource.");
+        }
+        return file;
     }
-
 }
