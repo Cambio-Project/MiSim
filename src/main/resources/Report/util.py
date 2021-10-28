3980b10f3352eaef0407ed501f0d49fc359ac007
LWagner <st148345@stud.uni-stuttgart.de>
2021-10-28 17:25:59 +0200

    added utility for two column output

--- src/main/resources/Report/util.py
+++ src/main/resources/Report/util.py
@@ -4,11 +4,11 @@ from typing import List, Tuple, Callable
 import matplotlib.pyplot as plt
 import sys
 from matplotlib.axes import Axes
+from matplotlib.gridspec import GridSpec
 from pandas import DataFrame
 
 
-def plot(datasets: List[Tuple[str, DataFrame]],
-         consumer: Callable[[Axes, DataFrame], None]):
+def _setup(datasets):
     duration = json.load(open("metadata.json"))["duration"]
 
     if len(datasets) == 0:
@@ -19,18 +19,44 @@ def plot(datasets: List[Tuple[str, DataFrame]],
     if len(sys.argv) > 1 and (sys.argv[1] == "--no-plot" or sys.argv[1] == "--headless"):
         exit(0)
 
-    fig = plt.figure(1)
     plt.tight_layout()
 
     step = max(int(duration / 10), 1)
-    x_ticks = range(0, int(duration + step), step)
+    x_ticks = range(0, int(duration + 1), step)
+    fig = plt.figure(1)
+
+    return fig, x_ticks
+
+
+def plot(datasets: List[Tuple[str, DataFrame]],
+         consumer: Callable[[Axes, DataFrame], None]):
+    fig, x_ticks = _setup(datasets)
 
     for index, (title, dataset) in enumerate(datasets, start=1):
-        ax = fig.add_subplot(len(datasets), 1, index)
+        ax = fig.add_subplot(1, 1, index)
         ax.set_title(title)
         ax.set_ylim(ymin=0, auto=True)
-        ax.set_xlim(xmin=0.1, xmax=duration)
+        ax.set_xlim(xmin=0.1, xmax=x_ticks.stop)
         ax.set_xticks(x_ticks)
         consumer(ax, dataset)
+
+    plt.show()
+
+
+def plot_two_column(datasets: List[Tuple[str, DataFrame]],
+                    consumer: Callable[[Axes, Axes, DataFrame], None]):
+    fig, x_ticks = _setup(datasets)
+
+    gs = GridSpec(len(datasets), 2, figure=fig)
+
+    for index, (title, dataset) in enumerate(datasets, start=0):
+        axes = [fig.add_subplot(gs[index, 0]),
+                fig.add_subplot(gs[index, 1])]
+        for ax in axes:
+            ax.set_title(title)
+            ax.set_ylim(ymin=0, auto=True)
+            ax.set_xlim(xmin=0.1, xmax=x_ticks.stop)
+            ax.set_xticks(x_ticks)
+        consumer(axes[0], axes[1], dataset)
 
     plt.show()
