a408df4c6a76ff0db3651f32d62bae27a549bce4
LWagner <st148345@stud.uni-stuttgart.de>
2021-10-28 12:27:25 +0200

    Extracted plot function into utility file

--- src/main/resources/Report/ResponseTimes.py
+++ src/main/resources/Report/ResponseTimes.py
@@ -1,14 +1,16 @@
 import glob
 import json
 import os
-import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from matplotlib.axes import Axes
+from pandas import DataFrame
+
+import util
 
 datasets = []
-duration = np.float32(json.load(open("metadata.json"))["duration"])
 
 for file in glob.glob(os.path.join(".", "raw", "*_ResponseTimes.csv")):
     data = pd.read_csv(file, sep=";",
@@ -37,30 +39,17 @@ for file in glob.glob(os.path.join(".", "raw", "*_ResponseTimes.csv")):
     endpoint_name = file[file.index("["):file.index("]")]
     datasets.append((endpoint_name, mean))
 
-if len(datasets) == 0:
-    print("No dataset found!")
-    exit(1)
 
-# use --no-plot for headless mode
-if len(sys.argv) > 1 and (sys.argv[1] == "--no-plot" or sys.argv[1] == "--headless"):
-    exit(0)
-
-fig = plt.figure(1)
-plt.tight_layout()
-
-maxTime = duration
-step = max(int(maxTime / 10), 1)
-x_ticks = range(0, int(duration + step), step)
-
-for index, (title, dataset) in enumerate(datasets, start=1):
-    ax = fig.add_subplot(len(datasets), 1, index)
-    ax.errorbar(dataset["simulation_time_rounded_down"], dataset
-    ["mean_of_bin"], dataset["error"], linestyle='None', marker='o')
+def write_dataset(ax: Axes, dataset: DataFrame):
+    ax.errorbar(dataset["simulation_time_rounded_down"],
+                dataset["mean_of_bin"],
+                dataset["error"],
+                linestyle='None',
+                marker='o')
     ax.plot(dataset["simulation_time_rounded_down"],
-            dataset["mean_of_bin"], linewidth=1)
-    ax.set_title(title)
-    ax.set_ylim(ymin=0)
-    ax.set_xlim(xmin=0.1)
-    ax.set_xticks(x_ticks)
+            dataset["mean_of_bin"],
+            linewidth=1)
 
-plt.show()
+
+util.plot(datasets, write_dataset)
+
---
+++ src/main/resources/Report/util.py
@@ -0,0 +1,37 @@
+import json
+from typing import List, Tuple, Callable
+
+import matplotlib.pyplot as plt
+import sys
+from matplotlib.axes import Axes
+from pandas import DataFrame
+
+
+def plot(datasets: List[Tuple[str, DataFrame]],
+         consumer: Callable[[Axes, DataFrame], None]):
+    duration = json.load(open("metadata.json"))["duration"]
+
+    if len(datasets) == 0:
+        print("No dataset found!")
+        exit(1)
+
+    # use --no-plot for headless mode
+    if len(sys.argv) > 1 and (sys.argv[1] == "--no-plot" or sys.argv[1] == "--headless"):
+        exit(0)
+
+    fig = plt.figure(1)
+    plt.tight_layout()
+
+    max_time = duration
+    step = max(int(max_time / 10), 1)
+    x_ticks = range(0, int(duration + step), step)
+
+    for index, (title, dataset) in enumerate(datasets, start=1):
+        ax = fig.add_subplot(len(datasets), 1, index)
+        ax.set_title(title)
+        ax.set_ylim(ymin=0)
+        ax.set_xlim(xmin=0.1)
+        ax.set_xticks(x_ticks)
+        consumer(ax, dataset)
+
+    plt.show()
