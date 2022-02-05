2435f2f3e324269d628b9f31fc69a6ab5bccb3af
Your Name <patti.haas@gmx.de>
2022-02-05 14:56:19 +0100

    Refactor Code

--- src/main/java/cambio/simulator/orchestration/scheduling/KubeScheduler.java
+++ src/main/java/cambio/simulator/orchestration/scheduling/KubeScheduler.java
@@ -13,6 +13,7 @@ import java.io.*;
 import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -23,6 +24,8 @@ import java.util.function.Consumer;
 public class KubeScheduler extends NamedEntity implements IScheduler {
 
     static String API_URL = "http://127.0.0.1:8000/update/";
+    static String PATH_PODS = "pods";
+    static String PATH_NODES = "nodes";
 
     private static int counter = 1;
 
@@ -39,7 +42,7 @@ public class KubeScheduler extends NamedEntity implements IScheduler {
 
         try {
             String nodeList = KubeJSONCreator.createNodeList(cluster.getNodes());
-            post(nodeList, 0, "nodes");
+            post(nodeList, 0, PATH_NODES);
 
 
         } catch (IOException e) {
@@ -57,36 +60,18 @@ public class KubeScheduler extends NamedEntity implements IScheduler {
         return SchedulerType.KUBE;
     }
 
+    //TODO Plugins für Scheduler testen, schreiben
     @Override
     public void schedulePods() {
         if (podWaitingQueue.isEmpty()){
             sendTraceNote(this.getQuotedName() + " has no pods left for scheduling");
             return;
         }
-
-        //TODO Plugins für Scheduler testen, schreiben
-        //TODO NodeAffinity testen
-
         try {
-
-
-//            String[] cmd_API = { "/bin/sh", "-c", "cd /home/ittaq/Documents/Uni/Masterarbeit/test/rest-api/fast-api; uvicorn main:app --reload" };
-//
-//            Process process_API = Runtime.getRuntime().exec(cmd_API);
-//
-//            StreamGobbler streamGobbler_API =
-//                    new StreamGobbler(process_API.getInputStream(), System.out::println);
-//            Executors.newSingleThreadExecutor().submit(streamGobbler_API);
-
             String[] cmd_Scheduler = {"/bin/sh", "-c", "cd /home/ittaq/Documents/Uni/Masterarbeit/scheduler; ./kube-scheduler --master 127.0.0.1:8000 --config config.txt"};
-
             Process process_Scheduler = Runtime.getRuntime().exec(cmd_Scheduler);
 
-
-
-
-
-            List podList = new ArrayList<>();
+            List<String> podList = new ArrayList<>();
             int numberOfPendingPods = podWaitingQueue.size();
             while (podWaitingQueue.size() != 0) {
                 String pendingPod = KubeJSONCreator.createPod(getNextPodFromWaitingQueue(), false);
@@ -102,8 +87,8 @@ public class KubeScheduler extends NamedEntity implements IScheduler {
                 String podListTemplateString = KubeJSONCreator.getPodListTemplate();
                 podListTemplateString = podListTemplateString.replace("TEMPLATE_RESOURCE_VERSION", String.valueOf(counter++));
                 podListTemplateString = podListTemplateString.replace("TEMPLATE_POD_LIST", podList.toString());
+                JSONObject response = post(podListTemplateString, numberOfPendingPods, PATH_PODS);
 
-                JSONObject response = post(podListTemplateString, numberOfPendingPods, "pods");
                 Map<String, Object> responseMap = response.toMap();
                 ArrayList<Map<String, String>> bindList = (ArrayList) responseMap.get("bindingList");
                 for (Map<String, String> map : bindList) {
@@ -120,7 +105,6 @@ public class KubeScheduler extends NamedEntity implements IScheduler {
                         throw new KubeSchedulerException("The pod that was selected by the kube-scheduler does not exist in the Simulation");
                     }
 
-
                     if (!candidateNode.addPod(pod)) {
                         throw new KubeSchedulerException("The selected node has not enough resources to run the selected pod. The kube-scheduler must have calculated wrong");
                     }
@@ -133,27 +117,29 @@ public class KubeScheduler extends NamedEntity implements IScheduler {
                     String podName = map.get("podName");
                     Pod pod = ManagementPlane.getInstance().getPodByName(podName);
                     podWaitingQueue.add(pod);
-                    sendTraceNote(this.getQuotedName() + " was not able to schedule pod " + pod + ". Unsufficient resources!");
+                    sendTraceNote(this.getQuotedName() + " was not able to schedule pod " + pod + ". Insufficient resources!");
                     sendTraceNote(this.getQuotedName() + " has send " + pod + " back to the Pod Waiting Queue");
                 }
             }
 
-            System.out.println();
-
             String[] cmd = {"/bin/sh", "-c", "kill `pidof kube-scheduler`"};
-            Process process_ = Runtime.getRuntime().exec(cmd);
+            Runtime.getRuntime().exec(cmd);
 
-//            process_API.destroy();
             process_Scheduler.destroy();
 
         } catch (IOException | KubeSchedulerException e) {
+            String[] cmd = {"/bin/sh", "-c", "kill `pidof kube-scheduler`"};
+            try {
+                Runtime.getRuntime().exec(cmd);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
             e.printStackTrace();
             System.exit(1);
         }
     }
 
     //    https://www.baeldung.com/httpurlconnection-post
-    //timeout verlängern
     public JSONObject post(String content, int numberPendingPods, String path) throws IOException {
         URL url = new URL(API_URL + path);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -167,15 +153,15 @@ public class KubeScheduler extends NamedEntity implements IScheduler {
         jsonInputString.put("data", content);
         jsonInputString.put("numberPendingPods", numberPendingPods);
         try (OutputStream os = con.getOutputStream()) {
-            byte[] input = jsonInputString.toString().getBytes("utf-8");
+            byte[] input = jsonInputString.toString().getBytes(StandardCharsets.UTF_8);
             os.write(input, 0, input.length);
         }
 
 
         try (BufferedReader br = new BufferedReader(
-                new InputStreamReader(con.getInputStream(), "utf-8"))) {
+                new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8))) {
             StringBuilder response = new StringBuilder();
-            String responseLine = null;
+            String responseLine;
             while ((responseLine = br.readLine()) != null) {
                 response.append(responseLine.trim());
             }
@@ -199,19 +185,4 @@ public class KubeScheduler extends NamedEntity implements IScheduler {
         return podWaitingQueue;
     }
 
-    private static class StreamGobbler implements Runnable {
-        private InputStream inputStream;
-        private Consumer<String> consumer;
-
-        public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
-            this.inputStream = inputStream;
-            this.consumer = consumer;
-        }
-
-        @Override
-        public void run() {
-            new BufferedReader(new InputStreamReader(inputStream)).lines()
-                    .forEach(consumer);
-        }
-    }
 }
