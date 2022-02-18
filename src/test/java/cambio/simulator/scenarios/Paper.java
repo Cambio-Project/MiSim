7b6842947e8070c90876ffeb6c34e5a67b989235
LWagner <st148345@stud.uni-stuttgart.de>
2022-02-18 11:21:51 +0100

    Added Test Resources for Paper Example

---
+++ src/test/java/cambio/simulator/scenarios/Paper.java
@@ -0,0 +1,33 @@
+package cambio.simulator.scenarios;
+
+import java.io.File;
+
+import cambio.simulator.testutils.FileLoaderUtil;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Executes the scenario and experiment for the running example of our paper.
+ *
+ * @author Lion Wagner
+ */
+@Disabled
+public class Paper {
+
+    @Test
+    void MinimalScenario() {
+        File scenario = FileLoaderUtil.loadFromExampleResources("PaperExample/paper_scenario.json");
+        File architecture = FileLoaderUtil.loadFromExampleResources("PaperExample/paper_architecture.json");
+        String[] args = new String[] {"-a", architecture.getAbsolutePath(), "-e", scenario.getAbsolutePath() };
+        cambio.simulator.Main.main(args);
+    }
+
+    @Test
+    void MinimalExperiment() {
+        File scenario = FileLoaderUtil.loadFromExampleResources("PaperExample/paper_experiment.json");
+        File architecture = FileLoaderUtil.loadFromExampleResources("PaperExample/paper_architecture.json");
+        String[] args =
+            new String[] {"-a", architecture.getAbsolutePath(), "-e", scenario.getAbsolutePath(), "-n", "1"};
+        cambio.simulator.Main.main(args);
+    }
+}
