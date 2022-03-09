e8ba19a577da233d02712bfcd9a7c704a00a7610
LWagner <st148345@stud.uni-stuttgart.de>
2022-03-09 08:13:49 +0100

    fixed scenario description not reading experiment/scenario name properly

--- src/main/java/cambio/simulator/parsing/adapter/scenario/ScenarioDescription.java
+++ src/main/java/cambio/simulator/parsing/adapter/scenario/ScenarioDescription.java
@@ -7,7 +7,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -38,6 +37,7 @@ import org.jetbrains.annotations.NotNull;
 @SuppressWarnings("unused")
 public final class ScenarioDescription {
 
+    @SerializedName(value = "name", alternate = {"scenarioName", "experiment_name"})
     private String name;
     private String description;
     private String artifact;
@@ -59,11 +59,11 @@ public final class ScenarioDescription {
      *
      * @throws ParsingException if artifact, component or stimulus are missing
      */
-    private void checkState() {
+    private void checkState(MiSimModel model) {
         if (StringUtils.isEmpty(stimulus)
             || StringUtils.isEmpty(artifact)
             || StringUtils.isEmpty(component)
-            || StringUtils.isEmpty(name)) {
+            || (StringUtils.isEmpty(name) && model.getExperimentMetaData().getExperimentName() == null)) {
             throw new ParsingException("Scenario is missing parts! (stimulus, artifact, component, name)");
         } else if (StringUtils.isBlank(environment)
             || StringUtils.isBlank(response)
@@ -79,7 +79,11 @@ public final class ScenarioDescription {
      * @return a set of objects that describe the scenario.
      */
     public ExperimentModel parse(MiSimModel model) {
-        this.checkState();
+        this.checkState(model);
+
+        if (this.name == null) {
+            this.name = model.getExperimentMetaData().getExperimentName();
+        }
 
         Set<ISelfScheduled> scheduables = new HashSet<>();
 
