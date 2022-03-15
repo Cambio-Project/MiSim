f0a08bf90356dfcb75d6e4264f2a79d1af96ca1a
LWagner <st148345@stud.uni-stuttgart.de>
2022-03-15 15:58:16 +0100

    Added precomputed naming to NamedEntity, -Event and -SimProcess
    This should improve performance slightly.

--- src/main/java/cambio/simulator/entities/NamedSimProcess.java
+++ src/main/java/cambio/simulator/entities/NamedSimProcess.java
@@ -9,6 +9,11 @@ import desmoj.core.simulator.SimProcess;
  * guaranteed to be unique.
  *
  * <p>
+ * Also pre-computes the names of the entity so the strings are not generated on every method call. This gives a
+ * performance improvement over the default implementation when read a large amount of names.
+ * </p>
+ *
+ * <p>
  * Plain names should be used when it comes to generating new entity names based on other entity names to prevent chains
  * of unique identifiers.
  *
@@ -17,24 +22,48 @@ import desmoj.core.simulator.SimProcess;
 public abstract class NamedSimProcess extends SimProcess {
 
     private String plainName;
+    private String quotedName;
+    private String quotedPlainName;
 
+    /**
+     * Constructs a new named non-repeating simulation process.
+     *
+     * @param model       The model this process belongs to.
+     * @param name        The name of this process.
+     * @param showInTrace Flag indicating whether the entity should be shown in the trace.
+     */
     public NamedSimProcess(Model model, String name, boolean showInTrace) {
         this(model, name, false, showInTrace);
     }
 
+    /**
+     * Constructs a new named simulation process.
+     *
+     * @param model       The model this process belongs to.
+     * @param name        The name of this process.
+     * @param repeating   If this process is automatically restart its lifecycle.
+     * @param showInTrace Flag indicating whether the entity should be shown in the trace.
+     */
     public NamedSimProcess(Model model, String name, boolean repeating, boolean showInTrace) {
         super(model, name, repeating, showInTrace);
         this.plainName = name;
+        this.quotedPlainName = "'" + name + "'";
+        this.quotedName = super.getQuotedName();
     }
 
     public String getPlainName() {
         return this.plainName;
     }
 
+    /**
+     * Renames this entity.
+     */
     @Override
     public void rename(String name) {
         super.rename(name);
         this.plainName = name;
+        this.quotedPlainName = "'" + name + "'";
+        this.quotedName = super.getQuotedName();
     }
 
     /**
@@ -43,6 +72,16 @@ public abstract class NamedSimProcess extends SimProcess {
      * @return the plain name of this entity surrounded with ' quotes.
      */
     public String getQuotedPlainName() {
-        return "'" + getPlainName() + "'";
+        return this.quotedPlainName;
+    }
+
+    /**
+     * Gets a quoted version of the name of this object. The name will include the object number assigned by DESMO-J.
+     *
+     * @return the name of this entity surrounded with ' quotes.
+     */
+    @Override
+    public String getQuotedName() {
+        return this.quotedName;
     }
 }
