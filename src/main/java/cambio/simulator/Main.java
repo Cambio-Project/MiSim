628ffc833415d03ac0f97bb3b6627b472df4fb4b
LWagner <st148345@stud.uni-stuttgart.de>
2022-03-15 15:40:28 +0100

    More entry options and Config naming improvments

--- src/main/java/cambio/simulator/Main.java
+++ src/main/java/cambio/simulator/Main.java
@@ -15,7 +15,10 @@ public final class Main {
     /**
      * Main entry point of the program.
      *
-     * @param args program options, see {@link CLI}
+     * @param args program options, see {@link ExperimentStartupConfig}
+     * @see #mainVarargs(String...)
+     * @see #startExperiment(String)
+     * @see #startExperiment(ExperimentStartupConfig)
      */
     public static void main(String[] args) {
 
@@ -30,10 +33,40 @@ public final class Main {
         startExperiment(startupConfig);
     }
 
-    public static void startExperiment(String cliString) {
-        main(cliString.split(" "));
+    /**
+     * Varargs variant of {@link #main(String[])}.
+     *
+     * @param args program options, see {@link ExperimentStartupConfig}
+     * @see #main(String[])
+     * @see #startExperiment(String)
+     * @see #startExperiment(ExperimentStartupConfig)
+     */
+    public static void mainVarargs(String... args) {
+        main(args);
     }
 
+    /**
+     * Starts an experiment, and uses the given string as cli arguments (splits on spaces). Use spaces only to separate
+     * arguments and not inside a value.
+     *
+     * @param cliString the cli argument string
+     * @see #main(String[])
+     * @see #mainVarargs(String...)
+     * @see #startExperiment(ExperimentStartupConfig)
+     */
+    public static void startExperiment(String cliString) {
+        main(cliString.replaceAll("\\s*", " ").split(" "));
+    }
+
+
+    /**
+     * Starts an experiment with the given {@link ExperimentStartupConfig}.
+     *
+     * @param startupConfig the experiment startup configuration
+     * @see #startExperiment(String)
+     * @see #main(String[])
+     * @see #mainVarargs(String...)
+     */
     public static void startExperiment(ExperimentStartupConfig startupConfig) {
         Experiment experiment = ExperimentCreator.createSimulationExperiment(startupConfig);
         System.out.printf("[INFO] Starting simulation at approximately %s%n", java.time.LocalDateTime.now());
