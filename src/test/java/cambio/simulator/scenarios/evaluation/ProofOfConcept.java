1f87fa01c39127ae10a36529308f677caf987fb7
LWagner <st148345@stud.uni-stuttgart.de>
2022-03-18 15:52:24 +0100

    Updated Proof of Concept Demonstrations to new version

--- src/test/java/cambio/simulator/scenarios/evaluation/ProofOfConcept.java
+++ src/test/java/cambio/simulator/scenarios/evaluation/ProofOfConcept.java
@@ -1,5 +1,8 @@
 package cambio.simulator.scenarios.evaluation;
 
+import static com.github.stefanbirkner.systemlambda.SystemLambda.catchSystemExit;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.io.File;
 
 import cambio.simulator.Main;
@@ -14,47 +17,54 @@ import org.junit.jupiter.api.Test;
 //@Execution(ExecutionMode.CONCURRENT), not working yet
 public class ProofOfConcept {
 
-    @Test
-    void AutoScaleDemonstration() {
-        File arch = FileLoaderUtil.loadFromExampleResources("ProofOfConcept", "arch_autoscale_demonstration.json");
-        File exp = FileLoaderUtil.loadFromExampleResources("ProofOfConcept", "exp_autoscale_demonstration.json");
-        String[] args = new String[] {"-a", arch.getAbsolutePath(), "-e", exp.getAbsolutePath(), "-t"};
-        Main.main(args);
+    static void runSimulation(int exitCode, File arch, File exp, String... additionalArgs) throws Exception {
+        int code = catchSystemExit(() -> {
+            String[] fileLocations = new String[] {"-a", arch.getAbsolutePath(), "-e", exp.getAbsolutePath()};
+            String[] allArgs = new String[additionalArgs.length + 4];
+            System.arraycopy(fileLocations, 0, allArgs, 0, fileLocations.length);
+            System.arraycopy(additionalArgs, 0, allArgs, 4, additionalArgs.length);
+            Main.main(allArgs);
+        });
+        assertEquals(code, exitCode);
     }
 
+
     @Test
-    void CircuitBreakerDemonstration() {
+    void CircuitBreakerDemonstration() throws Exception {
         File arch = FileLoaderUtil.loadFromExampleResources("ProofOfConcept", "arch_circuitbreaker_demonstration.json");
         File exp = FileLoaderUtil.loadFromExampleResources("ProofOfConcept", "exp_circuitbreaker_demonstration.json");
-        String[] args = new String[] {"-a", arch.getAbsolutePath(), "-e", exp.getAbsolutePath(), "-t"};
-        Main.main(args);
+        runSimulation(0, arch, exp, "-p", "-d", "-t");
     }
 
     @Test
-    void RetryConceptDemonstration() {
+    void RetryConceptDemonstration() throws Exception {
         File arch = FileLoaderUtil.loadFromExampleResources("ProofOfConcept", "arch_retry_demonstration.json");
         File exp = FileLoaderUtil.loadFromExampleResources("ProofOfConcept", "exp_retry_demonstration.json");
-        String[] args = new String[] {"-a", arch.getAbsolutePath(), "-e", exp.getAbsolutePath(), "-t"};
-        Main.main(args);
+        runSimulation(0, arch, exp, "-p", "-d", "-t");
     }
 
     @Test
-    void LoadBalancerRandomDemonstration() {
+    void RetryDisabledConceptDemonstration() throws Exception {
+        File arch = FileLoaderUtil.loadFromExampleResources("ProofOfConcept", "arch_retry_disabled_demonstration.json");
+        File exp = FileLoaderUtil.loadFromExampleResources("ProofOfConcept", "exp_retry_disabled_demonstration.json");
+        runSimulation(0, arch, exp, "-p", "-d", "-t");
+    }
+
+    @Test
+    void LoadBalancerRandomDemonstration() throws Exception {
         File arch = FileLoaderUtil.loadFromExampleResources("ProofOfConcept", "arch_loadbalancer_random_demonstration" +
             ".json");
-        File exp = FileLoaderUtil.loadFromExampleResources("ProofOfConcept", "exp_loadbalancer_demonstration" +
+        File exp = FileLoaderUtil.loadFromExampleResources("ProofOfConcept", "exp_loadbalancer_random_demonstration" +
             ".json");
-        String[] args = new String[] {"-a", arch.getAbsolutePath(), "-e", exp.getAbsolutePath(), "-t"};
-        Main.main(args);
+        runSimulation(0, arch, exp, "-p", "-d", "-t");
     }
 
     @Test
-    void LoadBalancerUtilDemonstration() {
+    void LoadBalancerUtilDemonstration() throws Exception {
         File arch = FileLoaderUtil.loadFromExampleResources("ProofOfConcept", "arch_loadbalancer_util_demonstration" +
             ".json");
-        File exp = FileLoaderUtil.loadFromExampleResources("ProofOfConcept", "exp_loadbalancer_demonstration" +
-            ".json");
-        String[] args = new String[] {"-a", arch.getAbsolutePath(), "-e", exp.getAbsolutePath(), "-t"};
-        Main.main(args);
+        File exp =
+            FileLoaderUtil.loadFromExampleResources("ProofOfConcept", "exp_loadbalancer_util_demonstration.json");
+        runSimulation(0, arch, exp, "-p", "-d", "-t");
     }
 }
