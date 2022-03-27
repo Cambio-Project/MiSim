14e01ff0288d05a6b8d2769696769d54dd025d76
LWagner <st148345@stud.uni-stuttgart.de>
2022-03-27 11:37:32 +0200

    Utilization Loadbalancer now distributes requests randomly in min bins

--- src/main/java/cambio/simulator/entities/patterns/UtilizationBalanceStrategy.java
+++ src/main/java/cambio/simulator/entities/patterns/UtilizationBalanceStrategy.java
@@ -1,9 +1,11 @@
 package cambio.simulator.entities.patterns;
 
-import java.util.Collection;
-import java.util.Comparator;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import cambio.simulator.entities.microservice.MicroserviceInstance;
+import cambio.simulator.entities.microservice.NoInstanceAvailableException;
+import cambio.simulator.models.MiSimModel;
 import cambio.simulator.parsing.JsonTypeName;
 
 /**
@@ -12,13 +14,38 @@ import cambio.simulator.parsing.JsonTypeName;
 @JsonTypeName("util")
 class UtilizationBalanceStrategy implements ILoadBalancingStrategy {
 
+    private Random rng = null;
+
     /**
      * Returns the instance of the given list, which currently has the lowest demand left.
      */
     @Override
     public MicroserviceInstance getNextInstance(Collection<MicroserviceInstance> runningInstances) {
-        return runningInstances.stream()
-            .min(Comparator.comparingDouble(MicroserviceInstance::getRelativeWorkDemand))
-            .orElse(null);
+        if (rng == null) {
+            createRNG(runningInstances);
+        }
+
+        if (runningInstances.isEmpty()) {
+            throw new NoInstanceAvailableException();
+        }
+
+        double min =
+            runningInstances.stream().mapToDouble(MicroserviceInstance::getRelativeWorkDemand).min().orElse(-1);
+
+        List<MicroserviceInstance> minimalInstances = runningInstances.stream()
+            .filter(instance -> instance.getRelativeWorkDemand() == min)
+            .collect(Collectors.toList());
+
+        final int index = rng.nextInt(minimalInstances.size());
+        final MicroserviceInstance microserviceInstance = minimalInstances.get(index);
+        return microserviceInstance;
+    }
+
+
+    private void createRNG(Collection<MicroserviceInstance> runningInstances) {
+        runningInstances.stream().findFirst().ifPresent(instance -> {
+            MiSimModel model = (MiSimModel) instance.getModel();
+            rng = new Random(model.getExperimentMetaData().getSeed());
+        });
     }
 }
