75f424c19b0dfc472f8756a9c2f9c8f3ddabe881
LWagner <st148345@stud.uni-stuttgart.de>
2022-03-27 20:42:27 +0200

    fixed wrong calculation of needed demand in autoscaler

--- src/main/java/cambio/simulator/entities/patterns/ReactiveAutoscalingPolicy.java
+++ src/main/java/cambio/simulator/entities/patterns/ReactiveAutoscalingPolicy.java
@@ -28,22 +28,21 @@ class ReactiveAutoscalingPolicy implements IAutoscalingPolicy {
 
     @Override
     public void apply(Microservice owner) {
-        List<Double> utils = owner.getRelativeUtilizationOfInstances();
         TimeInstant presentTime = owner.presentTime();
-        reporter.addDatapoint("_Util", presentTime, utils);
-
         int currentInstanceCount = owner.getInstancesCount();
+
         // double avg = owner.getUtilizationOfInstances().stream().mapToDouble(value -> value).average().orElse(0.0);
         double avg = owner.getAverageRelativeUtilization();
         if (currentInstanceCount <= 0) { //starts a instances if there are none
             owner.setInstancesCount(1);
         } else if (avg >= upperBound) {
-            owner.scaleToInstancesCount((int) (currentInstanceCount + Math.ceil(avg)));
+            double upScalingFactor = avg / (upperBound - 0.01);
+            owner.scaleToInstancesCount((int) Math.max(1, Math.ceil(currentInstanceCount * upScalingFactor)));
             lastScaleUp = presentTime;
         } else if (avg <= lowerBound
             && currentInstanceCount > 1
             && presentTime.getTimeAsDouble() - lastScaleUp.getTimeAsDouble() > holdTime) {
-            double downScaleFactor = (Math.max(0.01, avg) / lowerBound);
+            double downScaleFactor = Math.max(0.01, avg) / lowerBound;
             int newInstanceCount = (int) Math.max(1, Math.ceil(currentInstanceCount * downScaleFactor));
             owner.scaleToInstancesCount(newInstanceCount);
             lastScaleUp = presentTime;
