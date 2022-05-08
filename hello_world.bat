c2a6f2aa0d4281e1f53d7de0226ba48c609b23cd
LWagner <st148345@stud.uni-stuttgart.de>
2022-05-09 00:54:09 +0200

    added new default output for hello world scripts

--- hello_world.bat
+++ hello_world.bat
@@ -3,6 +3,6 @@
 call mvn -B clean package --file pom.xml "-DskipTests=true" "-Dmaven.javadoc.skip=true" "-Dcheckstyle.skipExec=true"
 
 
-java -jar ./target/misim.jar -a ./Examples/example_architecture_model.json -e ./Examples/example_experiment_chaosmonkey.json
+java -jar ./target/misim.jar -a ./Examples/example_architecture_model.json -e ./Examples/example_experiment_chaosmonkey.json -o ./HelloWorldResults
 
 exit %errorlevel%
