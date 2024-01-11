58bfb15a74e9e7b1be36a4c69bf7086bb0bb0ab6
Aref El-Maarawi <arefm.ven@gmail.com>
2024-01-11 02:47:54 +0100

    Add base spring boot app

---
+++ restAPI/src/test/java/restAPI/restAPI/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package restAPI.restAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
