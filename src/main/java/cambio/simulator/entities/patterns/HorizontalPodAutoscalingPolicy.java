3451ffa88173dd53ce2d99d46a4b6b7c114aa27d
franksn90 <s.frankausowen@googlemail.com>
2026-03-02 10:35:17 +0100

    Apply review comments
    Move comments to javadoc and give precedence to hard constraint checks (minimum instances, max instances) instead of aborting early due to tolerance area checks.

--- src/main/java/cambio/simulator/entities/patterns/HorizontalPodAutoscalingPolicy.java
+++ src/main/java/cambio/simulator/entities/patterns/HorizontalPodAutoscalingPolicy.java
@@ -6,13 +6,21 @@ import cambio.simulator.misc.TimeUtil;
 import cambio.simulator.parsing.JsonTypeName;
 import desmoj.core.simulator.TimeInstant;
 
+/**
+ * AN implementation based on Kubernetes
+ * <a href="https://github.com/kubernetes/kubernetes/blob/8caeec429ee1d2a9df7b7a41b21c626346b456fb/docs/design/horizontal-pod-autoscaler.md#autoscaling-algorithm">Horizontal Pod Autoscaler</a>.
+ * <p>
+ * By default, scale-up can only happen if there was no rescaling within the last 3 minutes. Scale-down will wait for 5
+ * minutes from the last rescaling. Moreover, any scaling will only be made if: avg(CurrentPodsConsumption) / Target
+ * drops below 0.9 or increases above 1.1 (10% tolerance)
+ * <p>
+ * TODO Maybe also include via adapter, upscaling/downscaling behavior not 100% as in Kubernetes, e.g. see
+ *      <a href="https://github.com/kubernetes/kubernetes/blob/master/pkg/apis/autoscaling/types.go#L113">HorizontalPodAutoscalerBehavior Implementation</a>
+ *      In particular, Kubernetes mentions different holdTimes for up- and downscaling, as well as a 30s evaluation
+ *      frequency (MiSim probably evaluates every time unit. Thus, reacts faster.)
+ */
 @JsonTypeName("hpa")
 public class HorizontalPodAutoscalingPolicy implements IAutoscalingPolicy {
-
-    // TODO Maybe also include via adapter, upscaling/downscaling behavior not 100% as in Kubernetes, e.g. see
-    //  HorizontalPodAutoscalerBehavior
-    // https://github.com/kubernetes/kubernetes/blob/master/pkg/apis/autoscaling/types.go#L113
-
     private transient MultiDataPointReporter reporter = null;
     private double targetUtilization = 0.8;
     private int minInstances = 1;
@@ -26,12 +34,6 @@ public class HorizontalPodAutoscalingPolicy implements IAutoscalingPolicy {
 
     @Override
     public void apply(Microservice owner) {
-        //https://github.com/kubernetes/kubernetes/blob/8caeec429ee1d2a9df7b7a41b21c626346b456fb/docs/design/horizontal-pod-autoscaler.md#autoscaling-algorithm
-//        Scale-up can only happen if there was no rescaling within the last 3 minutes. Scale-down will wait for 5
-//        minutes from the last rescaling.
-//        Moreover any scaling will only be made if: avg(CurrentPodsConsumption) / Target drops below 0.9 or
-//        increases above 1.1 (10% tolerance)
-
         if (reporter == null) {
             reporter = new MultiDataPointReporter(String.format("AS[%s]_", owner.getPlainName()), owner.getModel());
         }
@@ -40,9 +42,6 @@ public class HorizontalPodAutoscalingPolicy implements IAutoscalingPolicy {
         int currentInstanceCount = owner.getInstancesCount();
         double avg2Target = owner.getAverageRelativeUtilization() / targetUtilization;
         // Tolerance area
-        if (avg2Target > 0.9 && avg2Target < 1.1) {
-            return;
-        }
 
         int newInstanceCount = (int) Math.ceil(avg2Target * currentInstanceCount);
 
@@ -57,14 +56,17 @@ public class HorizontalPodAutoscalingPolicy implements IAutoscalingPolicy {
             owner.setInstancesCount(maxInstances);
             reporter.addDatapoint("Decision", presentTime, "Despawn");
             reporter.addDatapoint("InstanceChange", presentTime, maxInstances - currentInstanceCount);
+        } else if (avg2Target > 0.9 && avg2Target < 1.1) {
+            // Tolerance area
+            reporter.addDatapoint("Decision", presentTime, "Hold");
+            reporter.addDatapoint("InstanceChange", presentTime, 0);
         } else if (avg2Target > 1 && currentInstanceCount < maxInstances) {
             owner.scaleToInstancesCount(newInstanceCount);
             lastScaleUp = presentTime;
             reporter.addDatapoint("Decision", presentTime, "Up");
             reporter.addDatapoint("InstanceChange", presentTime, newInstanceCount - currentInstanceCount);
-        } else if (avg2Target < 1
-                && currentInstanceCount > minInstances
-                && TimeUtil.subtract(presentTime, lastScaleUp).getTimeAsDouble() > holdTime) {
+        } else if (avg2Target < 1 && currentInstanceCount > minInstances && TimeUtil.subtract(presentTime,
+                lastScaleUp).getTimeAsDouble() > holdTime) {
             owner.scaleToInstancesCount(newInstanceCount);
             lastScaleUp = presentTime;
             reporter.addDatapoint("Decision", presentTime, "Down");
